aa555891e2ac41abc9426d1507bb6bebde1949ec
ivan <ivan_ponomarew@mail.ru>
2022-05-17 19:20:22 +0400

    fix decorator

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from settings import created_by_celery, flask_app, mail
 from flask_mail import Message
 
 
-@created_by_celery.Task
+@created_by_celery.task()
 def send_mail(emails, email_data):
     for address in emails:
         msg = Message(email_data['subject'],
